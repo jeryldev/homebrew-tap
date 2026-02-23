9f3253bbbc0f71296656e74a526a57e2326cb6f7
jeryldev <jeryl.elixir.dev@outlook.com>
2026-02-24 07:35:27 +0800

    Fix dev-session-manager v2.1.3 SHA256 checksum

--- Formula/dev-session-manager.rb
+++ Formula/dev-session-manager.rb
@@ -2,7 +2,7 @@ class DevSessionManager < Formula
   desc "Quick development session bootstrapping with tmux"
   homepage "https://github.com/jeryldev/dev-session-manager"
   url "https://github.com/jeryldev/dev-session-manager/archive/refs/tags/v2.1.3.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  sha256 "635f79f9f7c9b37205ea16e334f599c30dcaccc5b86226c0e4239f65f25958fc"
   license "MIT"
 
   depends_on "tmux"
