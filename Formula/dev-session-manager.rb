247bba7a24ac9ceb0dbe585ee2e478317e98529c
jeryldev <jeryl.elixir.dev@outlook.com>
2026-06-07 17:28:34 +0800

    Update dev-session-manager to v2.3.0

--- Formula/dev-session-manager.rb
+++ Formula/dev-session-manager.rb
@@ -1,8 +1,8 @@
 class DevSessionManager < Formula
   desc "Quick development session bootstrapping with tmux"
   homepage "https://github.com/jeryldev/dev-session-manager"
-  url "https://github.com/jeryldev/dev-session-manager/archive/refs/tags/v2.2.0.tar.gz"
-  sha256 "79f6acc2997356046d52bb15fbc07e5be8aab16143b6800f141e6347cf03c5c9"
+  url "https://github.com/jeryldev/dev-session-manager/archive/refs/tags/v2.3.0.tar.gz"
+  sha256 "0e68b0a730ef9f7bf4bed8f283e84fe9925a68bce6eb33da7f639a7c03ed3082"
   license "MIT"
 
   depends_on "tmux"
